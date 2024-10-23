9edaac015e6674b88391d4559335c270080825c3
akayola <akayola@gmail.com>
2024-10-22 21:24:09 -0400

    finished chapter 6

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from app import login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -13,6 +14,9 @@ class User(UserMixin, db.Model):
 	username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
 	email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
 	password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+	about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+	last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+		default=lambda: datetime.now(timezone.utc))
 
 	posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
@@ -23,6 +27,11 @@ class User(UserMixin, db.Model):
 	
 	def check_password(self, password):
 		return check_password_hash(self.password_hash, password)
+	
+	
+	def avatar(self, size):
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		return f'https://gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 	
 	def __repr__(self):
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,15 +1,24 @@
+import sqlalchemy as sa
+from datetime import datetime, timezone
 from flask import request
+from flask import render_template, redirect, flash, url_for
 from urllib.parse import urlsplit
 from flask_login import login_required
 from flask_login import logout_user
 from flask_login import current_user, login_user
-import sqlalchemy as sa
-from app import db
-from app.forms import RegistrationForm
-from app.models import User
-from flask import render_template, redirect, flash, url_for
 from app import app
+from app import db
+from app.models import User
 from app.forms import LoginForm
+from app.forms import RegistrationForm
+from app.forms import EditProfileForm
+
+
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.now(timezone.utc)
+		db.session.commit()
 
 
 @app.route('/')
@@ -49,12 +58,6 @@ def login():
 	return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/logout')
-def logout():
-	logout_user()
-	return redirect(url_for('index'))
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
@@ -68,4 +71,36 @@ def register():
 		flash('Congratulations, you are now a registered user!')
 		return redirect(url_for('login'))
 	return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+	user = db.first_or_404(sa.select(User).where(User.username == username))
+	posts = [
+		{'author': user, 'body': 'Test post #1'},
+		{'author': user, 'body': 'Test post #2'}
+	]
+	return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
